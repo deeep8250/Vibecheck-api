f4cf0261725913f9b7fad24331b242ec632323b4
deeep8250 <deepmondal825005@gmail.com>
2026-05-27 07:53:17 +0530

    added redis client

--- db/redis/RedisInit.go
+++ db/redis/RedisInit.go
@@ -1,1 +1,27 @@
 package redisInit
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/deeep8250/vibecheck-api/internal/config"
+	"github.com/redis/go-redis/v9"
+)
+
+func RedisInit() {
+	addr := fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))
+	config.RedisClient = redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: os.Getenv("REDIS_PASSWORD"),
+	})
+
+	ctx := context.Background()
+	_, err := config.RedisClient.Ping(ctx).Result()
+	if err != nil {
+		log.Fatal("redis connection failed", err.Error())
+	}
+	log.Println("redis connection is successful")
+
+}
