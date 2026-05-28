5a7cd62fc43926af6b93dc770c48700a7fb43976
deeep8250 <deepmondal825005@gmail.com>
2026-05-28 09:47:07 +0530

    added a field

--- db/migrations/000002_create_posts.up.sql
+++ db/migrations/000002_create_posts.up.sql
@@ -5,5 +5,6 @@ create table if not exists posts(
     mood_tag varchar(200) not null,
     emoji varchar(10) not null,
     post_date date default current_date,
+    created_at timestamp default now(),
     unique(user_id,post_date)
 );
