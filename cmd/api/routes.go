5e4c180a9bf891d567293b7f40d74f1a1eaadea2
deeep8250 <deepmondal825005@gmail.com>
2026-06-02 10:53:14 +0530

    connected the post routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/deeep8250/vibecheck-api/internal/auth"
 	"github.com/deeep8250/vibecheck-api/internal/middleware"
+	"github.com/deeep8250/vibecheck-api/internal/post"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,6 +20,16 @@ func Routes() {
 
 	v1.POST("/register", authHandler.RegisterUser)
 	v1.POST("/login", authHandler.Login)
+
+	//post
+	postRepo := post.NewPostRepository()
+	postService := post.NewPostService(postRepo)
+	PostHandler := post.NewPostHandler(postService)
+
+	v1.POST("/post", middleware.Middleware(), PostHandler.CreatePostHandler)
+	v1.GET("/post/:id", middleware.Middleware(), PostHandler.GetPostHandler)
+	v1.PATCH("/post/:id", middleware.Middleware(), PostHandler.UpdatePostHandler)
+	v1.DELETE("/post/:id", middleware.Middleware(), PostHandler.DeletePostHandler)
 
 	r.Run(":8080")
 
